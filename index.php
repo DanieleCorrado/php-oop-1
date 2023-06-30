fab1de2159ceed8c0959fa5532d4cf44d1d95e47
DanieleCorrado <corrado_daniele@virgilio.it>
2023-06-30 14:24:17 +0200

    add genre array

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
      public $title;
     public $year;
     public $duration;
+    public $genre = array();
 
-    public function __construct($title, $year, $duration) {
+    public function __construct($title, $genre, $year, $duration) {
 
         $this -> title = $title;
+        $this -> genre = $genre;
         $this -> year = $year;
         $this -> duration = $duration;
     }
@@ -28,13 +30,26 @@
     return $this -> duration;
   }
 
+  public function MovieGenre() {
+
+    $genreList = "";
+
+
+    foreach ($this -> genre as $value) {
+      $genreList .= $value . " ";
+    }
+
+    return $genreList;
+    
+  }
+
 }
 
-  $movie1 = new Movie("Il Signore degli anelli - La compagnia dell'anello", 2001, 228);
-  $movie2 = new Movie("Ritorno al futuro", 1985, 92);
+  $movie1 = new Movie("Il Signore degli anelli - La compagnia dell'anello", ["fantasy", "Avventura"], 2001, 228);
+  $movie2 = new Movie("Ritorno al futuro", ["avventura"], 1985, 92);
 
   echo '<ul>';
-    echo '<li> Titolo: ' . $movie1 -> MovieTitle() . " Anno: " . $movie1 -> MovieYear() . " Durata: " . $movie1 -> MovieDuration() . ' Minuti</li>';
-    echo '<li> Titolo: ' . $movie2 -> MovieTitle() . " Anno: " . $movie2 -> MovieYear() . " Durata: " . $movie2 -> MovieDuration() . ' Minuti</li>';
+    echo '<li> Titolo: ' . $movie1 -> MovieTitle() . " Genere: " . $movie1 -> MovieGenre() . " Anno: " . $movie1 -> MovieYear() . " Durata: " . $movie1 -> MovieDuration() . ' Minuti</li>';
+    echo '<li> Titolo: ' . $movie2 -> MovieTitle() . " Genere: " . $movie2 -> MovieGenre() . " Anno: " . $movie2 -> MovieYear() . " Durata: " . $movie2 -> MovieDuration() . ' Minuti</li>';
   echo '</ul>';
 ?>
